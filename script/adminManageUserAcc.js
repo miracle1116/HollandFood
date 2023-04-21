27c188cff3b9a82c6cb50625c64571ee6dea77bf
Jaslene39 <U2102773@siswa.um.edu.my>
2023-04-22 01:55:18 +0800

    admin_manageUserAcc

--- script/adminManageUserAcc.js
+++ script/adminManageUserAcc.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
-    $('#accountTable').DataTable();
+  $("#accountTable").DataTable();
 });
+
+// $(function () {
+//   $("tr").on("click", function () {
+//     $(this).toggleClass("active");
+//   });
+// });
+
+// let listToggle = document.querySelector("#sidebarContent");
+
+// sectionTitle.addEventListener("click", function () {
+//   listToggle.classList.toggle("sideBar2");
+// });
