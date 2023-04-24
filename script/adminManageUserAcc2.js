f59aef7054d7c86ada5bacb50ecd245e6af8eb6a
Ong Shu Ying <ongshuyiing@gmail.com>
2023-04-24 15:35:18 +0800

    manage-user-acc-backup

---
+++ script/adminManageUserAcc2.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $("#accountTable").DataTable();
+  });
+  
+  // $(function () {
+  //   $("tr").on("click", function () {
+  //     $(this).toggleClass("active");
+  //   });
+  // });
+  
+  // let listToggle = document.querySelector("#sidebarContent");
+  
+  // sectionTitle.addEventListener("click", function () {
+  //   listToggle.classList.toggle("sideBar2");
+  // });
