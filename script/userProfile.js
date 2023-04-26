3fbc02a23ea23ce37236fd1de1d524f30f012fac
miracle1116 <yiquan1116@gmail.com>
2023-04-27 01:12:25 +0800

    User

--- script/userProfile.js
+++ script/userProfile.js
@@ -5,10 +5,13 @@ const error = document.getElementById('error');
 
 confirmPassword.addEventListener('input', () => {
     
-  if (password.value === confirmPassword.value) {
+  if (password.value === confirmPassword.value&&confirmPassword.value!="") {
     tick.style.display = 'inline-block';
     error.innerText = '';
-  } else {
+  } else if(password.value===""){
+    tick.style.display = 'none';
+    error.innerText = '';
+  }else {
     tick.style.display = 'none';
     error.innerText = 'Error: Inputs do not match';
   }
@@ -16,11 +19,48 @@ confirmPassword.addEventListener('input', () => {
 
 password.addEventListener('input', () => {
     
-    if (password.value === confirmPassword.value) {
+    if (password.value === confirmPassword.value&&confirmPassword.value!="") {
       tick.style.display = 'inline-block';
       error.innerText = '';
-    } else {
+    } else if(confirmPassword.value===""){
+      tick.style.display = 'none';
+      error.innerText = '';
+    }
+    else {
       tick.style.display = 'none';
       error.innerText = 'Error: Inputs do not match';
     }
+  });
+
+
+  //change profile pic 
+  const editIcon = document.getElementById('edit-icon');
+  const profileImage = document.getElementById('profile-image');
+
+  // Add an event listener to the edit icon
+  editIcon.addEventListener('click', () => {
+    // Create a file input element
+    const fileInput = document.createElement('input');
+    fileInput.type = 'file';
+
+    // Add an event listener to the file input element
+    fileInput.addEventListener('change', () => {
+      // Get the selected file from the file input element
+      const selectedFile = fileInput.files[0];
+
+      // Create a new FileReader object
+      const reader = new FileReader();
+
+      // Add an event listener to the FileReader object
+      reader.addEventListener('load', () => {
+        // Set the profile image source to the loaded image
+        profileImage.src = reader.result;
+      });
+
+      // Read the selected file as a data URL
+      reader.readAsDataURL(selectedFile);
+    });
+
+    // Click the file input element to open the file dialog box
+    fileInput.click();
   });
--- script/userSignInUp.js
+++ script/userSignInUp.js
@@ -10,3 +10,47 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+// to validate the input not empty
+function validateForm(event) {
+  event.preventDefault(); // prevent the form from submitting
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const error = document.getElementById('error');
+
+  if (email === "" ) {
+    error.innerText = 'Email is required field';
+
+  }else if (password===""){
+    error.innerText = 'Password is required field';
+  } else {
+    window.location.href = "/index.html";
+  }
+}
+
+
+// to validate the input not empty
+function validateSignUpForm(event) {
+  event.preventDefault(); // prevent the form from submitting
+
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email-signup").value;
+  const password = document.getElementById("password-signup").value;
+  const confirmPassword = document.getElementById("confirmPassword").value;
+  const error = document.getElementById('error-signup');
+
+  if (email ==="" ||username===""||password===""||confirmPassword==="") {
+    error.innerText = 'All field are required';
+  }else {
+    if(password!=confirmPassword){
+      error.innerText = 'Password not same';
+    }else{
+      container.classList.remove("sign-up-mode");
+    }
+  }
+}
+
+
+
+
