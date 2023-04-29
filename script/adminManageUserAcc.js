6b83877ef72c5d4001bcbfd2648a960fea042e1d
Ong Shu Ying <ongshuyiing@gmail.com>
2023-04-29 14:30:14 +0800

    manage-user-acc-adjust-table-header

--- script/adminManageUserAcc.js
+++ script/adminManageUserAcc.js
@@ -1,32 +1,15 @@
-// $(document).ready(function () {
-//   $("#accountTable").DataTable();
-// });
-
-// $(function () {
-//   $("tr").on("click", function () {
-//     $(this).toggleClass("active");
-//   });
-// });
-
-// let listToggle = document.querySelector("#sidebarContent");
-
-// sectionTitle.addEventListener("click", function () {
-//   listToggle.classList.toggle("sideBar2");
-// });
-
-const editModal = new bootstrap.Modal("#edit-acc-modal");
-const deleteModal = new bootstrap.Modal("#delete-acc-modal");
-const editBtns = document.querySelectorAll(".editBtn");
-const deleteBtns = document.querySelectorAll(".deleteBtn");
-
-editBtns.forEach(editBtn => {
-    editBtn.addEventListener("click",function(){
-        editModal.show();
-    });
-});
-
-deleteBtns.forEach(deleteBtn => {
-    deleteBtn.addEventListener("click",function(){
-        deleteModal.show();
-    });
-});
+$(document).ready(function () {
+    $("#accountTable").DataTable();
+  });
+  
+  // $(function () {
+  //   $("tr").on("click", function () {
+  //     $(this).toggleClass("active");
+  //   });
+  // });
+  
+  // let listToggle = document.querySelector("#sidebarContent");
+  
+  // sectionTitle.addEventListener("click", function () {
+  //   listToggle.classList.toggle("sideBar2");
+  // });
