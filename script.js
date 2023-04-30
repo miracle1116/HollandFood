d59281d5b26729bd438b0124da2f1548274c2c35
miracle1116 <yiquan1116@gmail.com>
2023-05-01 00:37:49 +0800

    simple-transition

--- script.js
+++ script.js
@@ -14,3 +14,70 @@ productContainers.forEach((item, i) => {
         item.scrollLeft -= containerWidth;
     })
 })
+
+
+// get the menu container, all menu cards, and the menu image
+const menuContainer = document.querySelector('.menu-container');
+const menuCards = document.querySelectorAll('.menu-card');
+const menuImage = menuContainer.querySelector('.food-icon');
+
+// add event listeners to each menu card
+menuCards.forEach((card) => {
+  const originalName = menuContainer.querySelector('.dish-name').textContent;
+  const originalInfo = menuContainer.querySelector('.dish-info').textContent;
+  const originalImage = menuImage.getAttribute('src');
+
+  card.addEventListener('mouseover', () => {
+    // get the data from the menu card
+    const name = card.querySelector('.menu-name').textContent;
+    const details = card.querySelector('.menu-details').textContent;
+    const image = card.querySelector('.menu-img').getAttribute('src');
+
+    // get the elements in the menu container that need to change
+    const dishRank = menuContainer.querySelector('.dish-rank');
+    const dishName = menuContainer.querySelector('.dish-name');
+    const dishInfo = menuContainer.querySelector('.dish-info');
+
+    // add a transition effect to the element changes
+    dishName.style.transition = 'all 0.5s';
+    dishInfo.style.transition = 'all 0.5s';
+    menuImage.style.transition = 'transform 0.5s';
+
+    // slide out the old image
+    menuImage.style.transform = 'translateX(-100%)';
+
+    setTimeout(() => {
+      // change the elements in the menu container
+      dishRank.textContent = '';
+      dishName.textContent = name;
+      dishInfo.textContent = details;
+      menuImage.setAttribute('src', image);
+
+      // slide in the new image
+      menuImage.style.transform = 'translateX(0%)';
+    }, 300);
+  });
+
+  card.addEventListener('mouseout', () => {
+    // remove the transition effect when the mouse leaves the menu card
+    const dishName = menuContainer.querySelector('.dish-name');
+    const dishInfo = menuContainer.querySelector('.dish-info');
+
+    dishName.style.transition = 'all 0.5s';
+    dishInfo.style.transition = 'all 0.5s';
+    menuImage.style.transition = 'transform 0.5s';
+
+    // slide out the new image
+    menuImage.style.transform = 'translateX(-100%)';
+
+    setTimeout(() => {
+      // reset the values of the elements in the menu container to their original values
+      dishName.textContent = originalName;
+      dishInfo.textContent = originalInfo;
+      menuImage.setAttribute('src', originalImage);
+
+      // slide in the original image
+      menuImage.style.transform = 'translateX(0%)';
+    }, 300);
+  });
+});
