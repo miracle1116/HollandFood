96a8a9adc64b0891d6a70354318d60b0ff9ae473
Ong Shu Ying <ongshuyiing@gmail.com>
2023-05-04 20:07:27 +0800

    change menu price to 2 decimal and add menu item

--- script/adminMenu.js
+++ script/adminMenu.js
@@ -109,37 +109,37 @@ addItemBtn.addEventListener("click", function () {
   addBtn.addEventListener("click", function () {
     // get the menu item name and image source
     // var itemCategory = document.querySelector("#add-item-modal #menu-category").value;
-    var itemName = document.querySelector(".itemName").value;
-    var itemPrice = document.querySelector(".item-price").value;
+    var itemName = document.querySelector(".addItemName").value;
+    var itemPrice = document.querySelector(".addPrice").value;
     var imageInput = document.querySelector("#categoryPhotoInput").getAttribute("src");
-    var itemIngredient = document.querySelector(".item-ingredient").value;
+    var itemIngredient = document.querySelector(".addIngredient").value;
    
     // create a new menu item card
     var menuItem =
-    '<div class="menu-item-container row d-flex justify-content-around mt-5 ms-2">'+
     '<div class="menu-card col-2 col-lg-2 col-sm-2 col-md-2 me-4 my-3" data-category="pizza">'+
-      '<div class="menu-item container-fluid shadow-lg bg-body">'+
-        '<img src="'+imageInput+'" class="menu-item rounded-circle shadow-lg bg-body" />'+
-      '</div>'+
-      '<div class="border_menu">'+
-        '<div class="menu-item-desc container-fluid">'+
-          '<div class="item-name fw-bold">'+itemName+'</div>'+
-          '<div class="ingredient">'+itemIngredient+'</div>'+
-          '<div class="mt-4 prBtns">'+
-            '<div class="price"> RM'+itemPrice+'</div>'+
-            '<div>'+
-              '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="deleteBtn bi bi-trash ms-auto" viewBox="0 0 16 16">'+
-                '<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>'+
-                '<path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>'+
-              '</svg>'+
-              '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="editBtn bi bi-pencil-fill ms-2" viewBox="0 0 16 16">'+
-              '<path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>'+
-              '</svg>'+
+            '<div class="menu-item container-fluid shadow-lg bg-body">'+
+              '<img src="'+imageInput+'" class="align-items-center w-2 img-fluid rounded mx-auto d-block mb-4 ms-4" />'+
             '</div>'+
-          '</div>'+
-        '</div>'+
-      '</div>'+
-    '</div>';
+            '<div class="border_menu">'+
+              '<div class="menu-item-desc container-fluid">'+
+                '<div class="item-name fw-bold">'+itemName+'</div>'+
+                '<div class="ingredient">'+itemIngredient+'</div>'+
+                '<div class="mt-4 prBtns">'+
+                  '<div class="price">RM'+itemPrice+'</div>'+
+                  '<div>'+
+                    '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="deleteBtn bi bi-trash ms-auto" viewBox="0 0 16 16">'+
+                      '<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />'+
+                      '<path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />'+
+                    '</svg>'+
+
+                    '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="editBtn bi bi-pencil-fill ms-2" viewBox="0 0 16 16">'+
+                      '<path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />'+
+                    '</svg>'+
+                  '</div>'+
+                '</div>'+
+              '</div>'+
+            '</div>'+
+          '</div>';
       
 
     // add the new menu item card to the end of the container
