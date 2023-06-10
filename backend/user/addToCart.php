1c6ed93ead94b6ce3803c0d1dca113dbe4a6c756
miracle1116 <yiquan1116@gmail.com>
2023-06-11 01:56:13 +0800

    user reservation make

--- backend/user/addToCart.php
+++ backend/user/addToCart.php
@@ -5,6 +5,6 @@ $menuID= $_POST['menuID'];
 if(isset($_SESSION['cart'][$menuID])){
     $_SESSION['cart'][$menuID]++;
 }else{
-    $_SESSION['cart'][$menuID]= "1";
+    $_SESSION['cart'][$menuID]= 1;
 }
 echo json_encode(['status' => 'success']);
