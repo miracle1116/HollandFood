4290d6760927593848f728e9b6a1ac101fc1a979
miracle1116 <yiquan1116@gmail.com>
2023-06-11 11:23:59 +0800

    time slot changes

--- backend/user/reservation.php
+++ backend/user/reservation.php
@@ -8,6 +8,7 @@ if(empty($note)){
   $note="-";
 }
 
+
 $userID = $_SESSION['userID'];
 $reservationDate = $_SESSION['reservationDate'];
 $reservationTime = $_SESSION['reservationTime'];
@@ -15,12 +16,14 @@ $tableNo = $_SESSION['tableNo'];
 $noOfPax = $_SESSION['noOfPax'];
 $reservedFood = serialize($_SESSION['cart']);
 
-
+$timeSlot = ['8:00-9:00','9:00-10:00','10:00-11:00','11:00-12:00','12:00-13:00','13:00-14:00',
+'14:00-15:00','15:00-16:00','16:00-17:00','17:00-18:00','18:00-19:00','19:00-20:00','20:00-21:00',
+'21:00-22:00'];
 
 $query = "INSERT INTO `reservation`(`userID`, `reservedDate`, `reservedTime`, `tableNo`, `paxNo`, `reservedFood`, `reservedNote`, `status`) VALUES (?,?,?
   ,?,?,?,?,?)";
 $stmt = $conn->prepare($query);
-$stmt->execute([$userID,$reservationDate,$reservationTime,$tableNo,$noOfPax,$reservedFood,$note,"Pending"]);
+$stmt->execute([$userID,$reservationDate,$timeSlot[$reservationTime],$tableNo,$noOfPax,$reservedFood,$note,"Pending"]);
 unset($_SESSION['reservationDate']);
 unset($_SESSION['reservationTime']);
 unset($_SESSION['tableNo']);
